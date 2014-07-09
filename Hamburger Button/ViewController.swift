958915c3a76cef3051b1fae023d52b607d80cb56
Dominique d'Argent <dominique.dargent@me.com>
2014-07-09 14:23:50 +0200

    Make HamburgerButton @IBDesignable

--- Hamburger Button/ViewController.swift
+++ Hamburger Button/ViewController.swift
@@ -9,23 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var button: HamburgerButton! = nil
+    @IBOutlet var button: HamburgerButton
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor(red: 38.0 / 255, green: 151.0 / 255, blue: 68.0 / 255, alpha: 1)
-
-        self.button = HamburgerButton(frame: CGRectMake(133, 133, 54, 54))
-        self.button.addTarget(self, action: "toggle:", forControlEvents:.TouchUpInside)
-
-        self.view.addSubview(button)
-    }
-
-    override func preferredStatusBarStyle() -> UIStatusBarStyle  {
-        return .LightContent
-    }
-
-    func toggle(sender: AnyObject!) {
+    @IBAction func toggle(sender: AnyObject!) {
         self.button.showsMenu = !self.button.showsMenu
     }
 }
