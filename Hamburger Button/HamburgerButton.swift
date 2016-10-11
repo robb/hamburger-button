412f0130f6db95c9ac2bd97e2e0ee13f60de2513
Carlos Rogelio Villanueva Ousset <crvo84@gmail.com>
2016-10-11 13:46:27 -0500

    Convert to Swift 3.0

--- Hamburger Button/HamburgerButton.swift
+++ Hamburger Button/HamburgerButton.swift
@@ -12,23 +12,23 @@ import UIKit
 
 class HamburgerButton : UIButton {
     let shortStroke: CGPath = {
-        let path = CGPathCreateMutable()
-        CGPathMoveToPoint(path, nil, 2, 2)
-        CGPathAddLineToPoint(path, nil, 28, 2)
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 2, y: 2))
+        path.addLine(to: CGPoint(x: 28, y:2))
 
         return path
     }()
 
     let outline: CGPath = {
-        let path = CGPathCreateMutable()
-        CGPathMoveToPoint(path, nil, 10, 27)
-        CGPathAddCurveToPoint(path, nil, 12.00, 27.00, 28.02, 27.00, 40, 27)
-        CGPathAddCurveToPoint(path, nil, 55.92, 27.00, 50.47,  2.00, 27,  2)
-        CGPathAddCurveToPoint(path, nil, 13.16,  2.00,  2.00, 13.16,  2, 27)
-        CGPathAddCurveToPoint(path, nil,  2.00, 40.84, 13.16, 52.00, 27, 52)
-        CGPathAddCurveToPoint(path, nil, 40.84, 52.00, 52.00, 40.84, 52, 27)
-        CGPathAddCurveToPoint(path, nil, 52.00, 13.16, 42.39,  2.00, 27,  2)
-        CGPathAddCurveToPoint(path, nil, 13.16,  2.00,  2.00, 13.16,  2, 27)
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 10, y: 27))
+        path.addCurve(to: CGPoint(x: 40, y: 27), control1: CGPoint(x: 12, y: 27), control2: CGPoint(x: 28.02, y: 27))
+        path.addCurve(to: CGPoint(x: 27, y: 02), control1: CGPoint(x: 55.92, y: 27), control2: CGPoint(x: 50.47, y: 2))
+        path.addCurve(to: CGPoint(x: 2, y: 27), control1: CGPoint(x: 13.16, y: 2), control2: CGPoint(x: 2, y: 13.16))
+        path.addCurve(to: CGPoint(x: 27, y: 52), control1: CGPoint(x: 2, y: 40.84), control2: CGPoint(x: 13.16, y: 52))
+        path.addCurve(to: CGPoint(x: 52, y: 27), control1: CGPoint(x: 40.84, y: 52), control2: CGPoint(x: 52, y: 40.84))
+        path.addCurve(to: CGPoint(x: 27, y: 2), control1: CGPoint(x: 52, y: 13.16), control2: CGPoint(x: 42.39, y: 2))
+        path.addCurve(to: CGPoint(x: 2, y: 27), control1: CGPoint(x: 13.16, y: 2), control2: CGPoint(x: 2, y: 13.16))
 
         return path
     }()
@@ -51,35 +51,35 @@ class HamburgerButton : UIButton {
         self.bottom.path = shortStroke
 
         for layer in [ self.top, self.middle, self.bottom ] {
-            layer.fillColor = nil
-            layer.strokeColor = UIColor.whiteColor().CGColor
-            layer.lineWidth = 4
-            layer.miterLimit = 4
-            layer.lineCap = kCALineCapRound
-            layer.masksToBounds = true
+            layer?.fillColor = nil
+            layer?.strokeColor = UIColor.white.cgColor
+            layer?.lineWidth = 4
+            layer?.miterLimit = 4
+            layer?.lineCap = kCALineCapRound
+            layer?.masksToBounds = true
 
-            let strokingPath = CGPathCreateCopyByStrokingPath(layer.path, nil, 4, .Round, .Miter, 4)
+            let strokingPath = CGPath(__byStroking: (layer?.path!)!, transform: nil, lineWidth: 4, lineCap: .round, lineJoin: .miter, miterLimit: 4)
 
-            layer.bounds = CGPathGetPathBoundingBox(strokingPath)
+            layer?.bounds = (strokingPath?.boundingBoxOfPath)!
 
-            layer.actions = [
+            layer?.actions = [
                 "strokeStart": NSNull(),
                 "strokeEnd": NSNull(),
                 "transform": NSNull()
             ]
 
-            self.layer.addSublayer(layer)
+            self.layer.addSublayer(layer!)
         }
 
-        self.top.anchorPoint = CGPointMake(28.0 / 30.0, 0.5)
-        self.top.position = CGPointMake(40, 18)
+        self.top.anchorPoint = CGPoint(x: 28.0 / 30.0, y: 0.5)
+        self.top.position = CGPoint(x: 40, y: 18)
 
-        self.middle.position = CGPointMake(27, 27)
+        self.middle.position = CGPoint(x: 27, y: 27)
        self.middle.strokeStart = hamburgerStrokeStart
         self.middle.strokeEnd = hamburgerStrokeEnd
 
-        self.bottom.anchorPoint = CGPointMake(28.0 / 30.0, 0.5)
-        self.bottom.position = CGPointMake(40, 36)
+        self.bottom.anchorPoint = CGPoint(x: 28.0 / 30.0, y: 0.5)
+        self.bottom.position = CGPoint(x: 40, y: 36)
     }
 
     var showsMenu: Bool = false {
@@ -120,16 +120,16 @@ class HamburgerButton : UIButton {
             if self.showsMenu {
                 let translation = CATransform3DMakeTranslation(-4, 0, 0)
 
-                topTransform.toValue = NSValue(CATransform3D: CATransform3DRotate(translation, -0.7853975, 0, 0, 1))
+                topTransform.toValue = NSValue(caTransform3D: CATransform3DRotate(translation, -0.7853975, 0, 0, 1))
                 topTransform.beginTime = CACurrentMediaTime() + 0.25
 
-                bottomTransform.toValue = NSValue(CATransform3D: CATransform3DRotate(translation, 0.7853975, 0, 0, 1))
+                bottomTransform.toValue = NSValue(caTransform3D: CATransform3DRotate(translation, 0.7853975, 0, 0, 1))
                 bottomTransform.beginTime = CACurrentMediaTime() + 0.25
             } else {
-                topTransform.toValue = NSValue(CATransform3D: CATransform3DIdentity)
+                topTransform.toValue = NSValue(caTransform3D: CATransform3DIdentity)
                 topTransform.beginTime = CACurrentMediaTime() + 0.05
 
-                bottomTransform.toValue = NSValue(CATransform3D: CATransform3DIdentity)
+                bottomTransform.toValue = NSValue(caTransform3D: CATransform3DIdentity)
                 bottomTransform.beginTime = CACurrentMediaTime() + 0.05
             }
 
@@ -144,14 +144,14 @@ class HamburgerButton : UIButton {
 }
 
 extension CALayer {
-    func ocb_applyAnimation(animation: CABasicAnimation) {
+    func ocb_applyAnimation(_ animation: CABasicAnimation) {
         let copy = animation.copy() as! CABasicAnimation
 
         if copy.fromValue == nil {
-            copy.fromValue = self.presentationLayer()!.valueForKeyPath(copy.keyPath!)
+            copy.fromValue = self.presentation()!.value(forKeyPath: copy.keyPath!)
         }
 
-        self.addAnimation(copy, forKey: copy.keyPath)
+        self.add(copy, forKey: copy.keyPath)
         self.setValue(copy.toValue, forKeyPath:copy.keyPath!)
     }
 }
